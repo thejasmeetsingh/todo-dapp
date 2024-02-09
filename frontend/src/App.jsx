8df215df72e271da92bf1b3af5fd133af52c3cc3
Jasmeet Singh <51269010+thejasmeetsingh@users.noreply.github.com>
2024-02-09 15:16:52 +0000

    Added Add task form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
+import AddTask from "./components/AddTask";
+
 const App = () => {
   return (
     <div>
       <h1>TODO: DApp</h1>
+      <div>
+        <AddTask />
+      </div>
     </div>
   );
 };
--- frontend/src/components/AddTask.jsx
+++ frontend/src/components/AddTask.jsx
@@ -1,3 +1,11 @@
 export default function AddTask() {
-  return <div>Add Task</div>;
+  return (
+    <div>
+      <form>
+        <input autoFocus placeholder="Title" />
+        <input placeholder="Description" />
+        <button type="submit">Add Task</button>
+      </form>
+    </div>
+  );
 }
