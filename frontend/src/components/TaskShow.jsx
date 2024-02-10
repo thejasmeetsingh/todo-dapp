36f41b4798e6b91e9bb0ad0e1d6e1ef5d4109e8a
Jasmeet Singh <51269010+thejasmeetsingh@users.noreply.github.com>
2024-02-10 10:24:49 +0000

    Added task delete flow

--- frontend/src/components/TaskShow.jsx
+++ frontend/src/components/TaskShow.jsx
@@ -1,9 +1,18 @@
+import useTaskContext from "../hooks/use-task-context";
+
 export default function TaskShow({ idx, task }) {
+  const { deleteTask } = useTaskContext();
+
+  const handleDelete = () => {
+    deleteTask(idx);
+  };
+
   return (
     <div>
       <p>Index: {idx}</p>
       <p>Title: {task.title}</p>
       <p>Description: {task.description}</p>
+      <button onClick={handleDelete}>Delete</button>
     </div>
   );
 }
